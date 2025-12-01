5c0ebdc604e7f3069a7acd86c95da8257c585516
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-12-01 19:53:55 +0100

    vue routing + store + tailwindcss

---
+++ web/src/pages/Home.vue
@@ -0,0 +1,6 @@
+<template>
+  <RouterLink to="/test">test page</RouterLink>
+  <div class="flex items-center justify-center min-h-screen">
+    <p>Home Page</p>
+  </div>
+</template>
---
+++ web/src/pages/Test.vue
@@ -0,0 +1,4 @@
+<template>
+  <RouterLink to="/home">home page</RouterLink>
+  <div class="flex items-center justify-center min-h-screen">Test Page</div>
+</template>
