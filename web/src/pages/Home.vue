d54ec6e1957168b48360752eba71f331298a32ed
bm-lamine <bennouioua.mohammed.lamine@gmail.com>
2025-12-02 13:05:55 +0100

    no more VueJs

--- web/src/pages/Home.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <RouterLink to="/test">test page</RouterLink>
-  <div class="flex items-center justify-center min-h-screen">
-    <p>Home Page</p>
-  </div>
-</template>
--- web/src/pages/Test.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <RouterLink to="/home">home page</RouterLink>
-  <div class="flex items-center justify-center min-h-screen">Test Page</div>
-</template>
